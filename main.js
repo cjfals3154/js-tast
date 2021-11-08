0f82d87fbf9e4921664095e9ea8181b7f646e53b
cjfals3154 <cjfals3154@naver.com>
2021-11-08 23:37:51 +0900

    js 공부

---
+++ main.js
@@ -0,0 +1,92 @@
+//반복문
+// for (시작조건; 종료조건; 변화조건){}
+
+// const ulEl = document.querySelector('ul')
+
+// for (let i = 0; i < 10; i += 1){
+//     const li = document.createElement('li')
+//     li.textContent = `list-${i + 1}`
+//   if((i+1) % 2 === 0){
+//     li.addEventListener('click', function(){
+//       console.log(li.textContent)
+//     })
+//   }
+//     ulEl.appendChild(li)
+//   }
+
+
+
+// import random from './getRandom'
+
+// const a =random()
+
+//스위치
+
+// switch(a){
+//   case 0:
+//     console.log('a is 0')
+//     break
+//   case 2:
+//     console.log('a is 2')
+//     break
+//   case 4:
+//     console.log('a is 4')
+//     break
+//   default:
+//     console.log('rest...')
+// }
+
+//조건문
+
+// if (a===0){
+//   console.log('a is 0')
+// } else if(a === 2){
+//   console.log('a is 2')
+// } else if(a === 4){
+//   console.log('a is 4')
+// }  else {
+//   console.log('rest...')
+// }
+
+
+// 할당 연산자 (assignment operator)
+
+// let a = 2
+// // a = a + 1
+// a -= 1
+// console.log(a)
+
+// 비교 연산자 (comparsion operator)
+// const a = 5
+// const b = 5
+
+// console.log(a >= b)
+
+
+//논리 연산자 (logical operator)
+// const a = 1 === 13
+// const b = 'AB' === 'ABC'
+// const c = false
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// console.log('&&:', a && b && c)
+// console.log('||:', a || b || c)
+// console.log('!', !a)
+
+
+// 삼항 연산자 (ternary operator)
+
+// const a = 1 < 2
+
+// if (a) {
+//   console.log('참')
+// } else{
+//   console.log('거짓')
+// }
+
+// console.log(a ? '참' : '거짓')
+
+
