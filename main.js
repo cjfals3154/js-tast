51fd338cc1edb202b9ceadfeb36e174623c9685c
Shincheolmin <90400739+cjfals3154@users.noreply.github.com>
2021-11-10 15:46:12 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,42 @@
-//함수 복습
-//매개 변수
-function sum(){
-  console.log(arguments)
-  return
+function user(first, last) {
+  this.firstName = first
+  this.lastName = last
+}
+user.prototype.getFullName = function (){
+  return `${this.firstName} ${this.lastName}`
 }
 
-console.log(sum(7,3))
+const heropy = new user('Heropy', 'Park')
+const amy = new user('Amy', 'Clarke')
+const neo = new user('Neo', 'Smith')
+
+console.log(heropy.getFullName())
+console.log(amy.getFullName())
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+//함수 복습
+//매개 변수
+// function sum(){
+//   console.log(arguments)
+//   return
+// }
+
+// console.log(sum(7,3))
 
 
 
