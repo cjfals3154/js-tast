86a583e25ddc3aec75c7e326b0c2d1bb193e9749
Shincheolmin <90400739+cjfals3154@users.noreply.github.com>
2021-11-13 23:34:42 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,93 @@
-function user(first, last) {
-  this.firstName = first
-  this.lastName = last
-}
-user.prototype.getFullName = function (){
-  return `${this.firstName} ${this.lastName}`
-}
-
-const heropy = new user('Heropy', 'Park')
-const amy = new user('Amy', 'Clarke')
-const neo = new user('Neo', 'Smith')
-
-console.log(heropy.getFullName())
-console.log(amy.getFullName())
+//배열
+// .length (배열에 아이템의 개수를 출력)
+// .concat() (원본의 배열 데이터에는 영향이 없고 새로운 데이터가 합쳐져서 출력 됨)
+// .forEach() (메소드에 붙어있는 아이템의 개수 만큼 콜백함수가 반복적으로 실행 됨)
+// .map() (콜백에서 반환 된 특정한 데이터를 기준으로해서 데이터들을 반환)
 
 
 
+//숫자와 수학
+// toFixed(2) (소수점 2번째 자리까지 남겨두고 지우기 but 문자 데이터로 출력 됨)
+// parseInt 전역함수(정수만 숫자 데이터로 출력 됨)
+// parseFloat 전역함수(소수점 자리의 숫자도 유지하면서 숫자 데이터로 출력 됨)
+// abs (숫자의 절대값만 출력 됨)
+// min (가장 작은 값만 출력 됨)
+// max (가장 큰 값만 출력 됨)
+// ceil (숫자 올림처리)
+// floor (숫자 내림처리)
+// round (반올림)
+
+
+//문자
+// length(글자가 몇개가 있는지),
+// indexOf(글자가 몇번째 위치해 있는지 없으면 -1이 뜸 이걸로 ture, f를 조정가능)
+// slice(어디서 부터 어디까지 글자를 출력하기 ex) 0, 4)
+// replace(바꾸고 싶은 문자와 새로 추가 될 문자를 입력하여 교환한다. )
+// match(정규표현식 (str.match(/.+(?=@)/)) )
+// trim(문자 데이터에 맨앞, 맨뒤의 공백문자 제거)
+
+
+// class Vehicle{
+//   constructor(name, wheel){
+//     this.name = name
+//     this.wheel = wheel
+//   }
+// }
+// const myVehicle = new Vehicle('이동수단', 2)
+// console.log(myVehicle)
+// class Bicycle extends Vehicle {
+//   constructor(name, wheel) {
+//     super(name, wheel)
+//   }
+// }
+// const myBicycle = new Bicycle('삼천리', 2)
+// const daughtersBicycle = new Bicycle('세발', 3)
+// console.log(myBicycle)
+// console.log(daughtersBicycle)
+// class Car extends Vehicle {
+//   constructor(name, wheel, license){
+//     super(name, wheel)
+//     this.license = license
+//   }
+// }
+// const myCar = new Car('벤츠', 4, true)
+// const daughtersCar = new Car('포르쉐', 4, false)
+
+
+// console.log(myCar)
+// console.log(daughtersCar)
+
+//ES6 Classes
+
+// const heropy = {
+//   name: 'Heropy',
+//   normal(){
+//     console.log(this.name)
+//   },
+//   arrow: () =>{
+//     console.log(this.name)
+//   }
+// }
+
+// heropy.normal()
+// heropy.arrow()
 
 
 
+// function user(first, last) {
+//   this.firstName = first
+//   this.lastName = last
+// }
+// user.prototype.getFullName = function (){
+//   return `${this.firstName} ${this.lastName}`
+// }
 
+// const heropy = new user('Heropy', 'Park')
+// const amy = new user('Amy', 'Clarke')
+// const neo = new user('Neo', 'Smith')
 
-
-
-
-
+// console.log(heropy.getFullName())
+// console.log(amy.getFullName())
 
 
 
