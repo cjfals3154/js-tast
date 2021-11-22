f1d513a1fa0f17b9633eb42338ebc34d907307e5
Shincheolmin <90400739+cjfals3154@users.noreply.github.com>
2021-11-22 23:21:48 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
-import _ from 'lodash'
 
-const usersA = [
-  {userId: '1', name: 'shin'},
-  {userId: '2', name: 'Neo'}
-]
-const usersB = [
-  {userId: '1', name: 'shin'},
-  {userId: '3', name: 'Amy'}
-]
-const usersC = usersA.concat(usersB)
-console.log('concat', usersC)
-console.log('uniqBy', _.uniqBy(usersC, 'userId'))
 
-const usersD = _.unionBy(usersA, usersB, 'userId')
-console.log('unionBy', usersD)
+
+
+
+
+
+
+// import _ from 'lodash'
+
+// const usersA = [
+//   {userId: '1', name: 'shin'},
+//   {userId: '2', name: 'Neo'}
+// ]
+// const usersB = [
+//   {userId: '1', name: 'shin'},
+//   {userId: '3', name: 'Amy'}
+// ]
+// const usersC = usersA.concat(usersB)
+// console.log('concat', usersC)
+// console.log('uniqBy', _.uniqBy(usersC, 'userId'))
+
+// const usersD = _.unionBy(usersA, usersB, 'userId')
+// console.log('unionBy', usersD)
 
 
 // import Type from './getType'
